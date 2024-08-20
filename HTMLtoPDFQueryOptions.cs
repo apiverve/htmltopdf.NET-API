2e01976b0c30eb59eea60bef22e0c76358ef3cbb
APIVerve Team <hello@apiverve.com>
2024-08-20 10:55:16 -0500

    Bug Fixes

---
+++ HTMLtoPDFQueryOptions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class HTMLtoPDFQueryOptions {
+public string html { get; set; }
+public double marginTop { get; set; }
+public double marginRight { get; set; }
+public double marginBottom { get; set; }
+public double marginLeft { get; set; }
+}
+
+}
